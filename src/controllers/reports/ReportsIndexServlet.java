2d13dc32c38434dd0b646486320b57ae7a1eb779
kana <kana.kibayashi@gmail.com>
2021-02-22 03:38:21 +0900

    update Report Servlrt

--- src/controllers/reports/ReportsIndexServlet.java
+++ src/controllers/reports/ReportsIndexServlet.java
@@ -1,7 +1,9 @@
 package controllers.reports;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+import models.Follow;
 import models.Report;
 import utils.DBUtil;
 
@@ -33,9 +37,12 @@ public class ReportsIndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
         EntityManager em = DBUtil.createEntityManager();
+        Employee login_employee = (Employee) request.getSession().getAttribute("login_employee");
 
         int page;
+        Map<Integer, Boolean> followMap = new HashMap<Integer, Boolean>();
         try {
             page = Integer.parseInt(request.getParameter("page"));
         } catch (Exception e) {
@@ -45,22 +52,49 @@ public class ReportsIndexServlet extends HttpServlet {
                 .setFirstResult(15 * (page - 1))
                 .setMaxResults(15)
                 .getResultList();
-
         long reports_count = (long) em.createNamedQuery("getReportsCount", Long.class)
                 .getSingleResult();
 
-        em.close();
-
-        request.setAttribute("reports", reports);
-        request.setAttribute("reports_count", reports_count);
-        request.setAttribute("page", page);
-        if (request.getSession().getAttribute("flush") != null) {
-            request.setAttribute("flush", request.getSession().getAttribute("flush"));
-            request.getSession().removeAttribute("flush");
+        for (Report report : reports) {
+            try {
+                Follow f = em.createNamedQuery("findIsFollowByUser", Follow.class)
+                        .setParameter("user_id", login_employee)
+                        .setParameter("follow_id", report.getEmployee())
+                        .getSingleResult();
+                if (f != null) {
+                    followMap.put(report.getId(), true);
+                }
+            } catch (Exception e) {
+                followMap.put(report.getId(), false);
+            }
         }
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/index.jsp");
-        rd.forward(request, response);
-    }
+            /*good　とりあえずコメントアウト
+        Map<Integer, Long> goodMap = new HashMap<Integer, Long>();
 
-}
+
+        for (Report report :reports) {
+
+            Long goods_count = (Long)em.createNamedQuery("getMyGoodCount", Long.class)
+                    .setParameter("report_id", report.getEmployee())
+                    .getSingleResult();
+
+            goodMap.put(report.getId(),goods_count);
+        }
+             */
+
+            em.close();
+
+            //request.setAttribute("goodMap", goodMap);
+            request.setAttribute("followMap", followMap);
+            request.setAttribute("reports", reports);
+            request.setAttribute("reports_count", reports_count);
+            request.setAttribute("page", page);
+            if (request.getSession().getAttribute("flush") != null) {
+                request.setAttribute("flush", request.getSession().getAttribute("flush"));
+                request.getSession().removeAttribute("flush");
+            }
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/index.jsp");
+            rd.forward(request, response);
+        }
+    }
--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -1,5 +1,4 @@
 package controllers.reports;
-
 import java.io.IOException;
 
 import javax.persistence.EntityManager;
@@ -10,39 +9,67 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+import models.Follow;
+import models.Good;
 import models.Report;
 import utils.DBUtil;
-
 /**
  * Servlet implementation class ReportsShowServlet
  */
 @WebServlet("/reports/show")
 public class ReportsShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ReportsShowServlet() {
         super();
     }
-
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
-
         Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+
+        Good g = new Good();
+        try{
+            g = em.createNamedQuery("findIsGoodByUser", Good.class)
+                        .setParameter("user_id", login_employee)
+                        .setParameter("report_id", r)
+                        .getSingleResult();
+        }catch(Exception e){
+            g = null;
+        }
+
+        Follow f = new Follow();
+        try{
+            f = em.createNamedQuery("findIsFollowByUser", Follow.class)
+                        .setParameter("user_id", login_employee)
+                        .setParameter("follow_id", r.getEmployee())
+                        .getSingleResult();
+        }catch(Exception e){
+            f = null;
+        }
+
+        long goods_count = (long) em.createNamedQuery("getMyGoodCount", Long.class)
+                .setParameter("report_id", r)
+                .getSingleResult();
 
         em.close();
-
+        request.setAttribute("goods_count", goods_count);
+        request.setAttribute("follow", f);
+        request.setAttribute("good", g);
         request.setAttribute("report", r);
         request.setAttribute("_token", request.getSession().getId());
-
+        if(request.getSession().getAttribute("flush") != null) {
+        request.setAttribute("flush", request.getSession().getAttribute("flush"));
+        request.getSession().removeAttribute("flush");
+        }
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
         rd.forward(request, response);
     }
-
 }
